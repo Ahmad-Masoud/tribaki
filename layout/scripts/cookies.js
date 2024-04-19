cc64cfaabd9c8d50dbb2aa7603f836fc7b8a75e7
Ahmad Masoud <72744090+Ahmad-Masoud@users.noreply.github.com>
2024-04-19 19:04:55 +0500

    Update cookies.js

--- layout/scripts/cookies.js
+++ layout/scripts/cookies.js
@@ -2,7 +2,7 @@ function setCookie(cname, cValue, expdays) {
     const d = new Date();
     d.setTime(d.getTime() + (expdays * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cValue + ";" + expires + ";path=/, path=/pages/contactform ,path=/pages/galleryspace , path=/pages/gallerytech , path=/space/betelgeuse , path=/space/Euro-jupiter , path=/space/jwst , path=/space/jwstwse , path=/space/kdtmn , path=/space/NASAPsyche , path=/space/OSIRIS , path=/space/SME , path=/space/SpaceXandBoeing , path=/tech/CCBMA_12, path=/tech/CDDGIS, path=/tech/chinacanalsp , path=/tech/CPUWAR, path=/tech/DRCEN, path=/tech/FMDPGAI, path=/tech/NandM, path=/tech/openai, path=/tech/RSAConf";
+    document.cookie = cname + "=" + cValue + ";" + expires + ";path=/";
   }
 
   function getCookie(cname) {
